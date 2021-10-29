e5be0ed10f727671db262a76acf8ecba0b2a89d4
Sumaiya Garag <sumaiya.garag@gmail.com>
2021-10-29 19:43:54 +0530

    Anagrams

---
+++ anagrams.js
@@ -0,0 +1,23 @@
+var words=['rope','pore','repo','red rum','murder','listen','silent','endeavour'];
+var anagrams=[];
+var output=[];
+
+for(var i in words){
+    var word=words[i];
+    var sortedWord= sortWord(word);
+    
+    if(anagrams[sortedWord]!=null)
+        anagrams[sortedWord].push(word);
+    else
+        anagrams[sortedWord]=[word];   
+}
+
+for (var sorted in anagrams) {
+    output.push(anagrams[sorted]);
+}
+
+console.log(output);
+
+function sortWord(word){
+    return word.replace(/ +/g,"").split('').sort().join('');
+}
