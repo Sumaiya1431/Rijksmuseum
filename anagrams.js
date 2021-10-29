f809e7df6989865b5fe14c835fad5f788f4d04c5
Sumaiya Garag <sumaiya.garag@gmail.com>
2021-10-29 20:34:09 +0530

    Added validations

--- anagrams.js
+++ anagrams.js
@@ -1,23 +1,35 @@
-var words=process.argv[2].split(',');
 var anagrams=[];
 var output=[];
 
-for(var i in words){
-    var word=words[i];
-    var sortedWord= sortWord(word);
-
-    if(anagrams[sortedWord]!=null)
-        anagrams[sortedWord].push(word);
-    else
-        anagrams[sortedWord]=[word];   
+if(process.argv[2]!=null){
+    var words=process.argv[2].split(',');
+    createAnagrams(words);
+    console.log(output);
+}
+else{
+    console.log("Usage: node anagrams.js \"<input>\" \nExample: node anagrams.js \"rope,pore,repo,red rum,murder,listen,silent,endeavour\" ");
 }
 
-for (var sorted in anagrams) {
-    output.push(anagrams[sorted]);
+function createAnagrams(words){
+
+    for(var i in words){
+        var word=words[i];
+        var sortedWord= sortWord(word);
+    
+        if(anagrams[sortedWord]!=null)
+            anagrams[sortedWord].push(word);
+        else
+            anagrams[sortedWord]=[word];   
+    }
+    
+    for (var sorted in anagrams) {
+        output.push(anagrams[sorted]);
+    }
 }
 
 
-console.log(output);
+
+
 
 function sortWord(word){
     return word.replace(/ +/g,"").split('').sort().join('');
